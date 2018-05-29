7da46f20b0e9f99a7aeab27ad7ca561c5284f11d
Nikolai Seip <nikolai-seip@web.de>
2018-05-29 09:12:35 +0200

    erkennt nun auch den Ausschnitt eines Bildes

--- src/Aufgabe3Fingerprint/Aufgabe3FingerPrintTest.java
+++ src/Aufgabe3Fingerprint/Aufgabe3FingerPrintTest.java
@@ -11,50 +11,78 @@ import javax.imageio.ImageIO;
 
 public class Aufgabe3FingerPrintTest {
 
-	public final static String fileLocationFirstPic = "src\\pictures\\BildWordNurAstloecher.bmp";
-	public final static String fileLocationSecondPic = "src\\pictures\\BildWordNurAstloecherGedreht.bmp";
-
+	public final static String fileLocationBasicPicture = "src\\pictures\\BildWordNurAstloecher.bmp";
+	public final static String fileLocationRotatedPicture = "src\\pictures\\BildWordNurAstloecherGedreht.bmp";
+	public final static String fileLocationSegmentPicture = "src\\pictures\\BildWordNurAstloecherAusschnitt.bmp";
 	
 	public static void aufgabe3FingerprintTest() {
 		
 		//Lese Bilder ein		
-		BufferedImage img1 = null;
-		BufferedImage img2 = null;
+		BufferedImage basis = null;
+		BufferedImage rotated = null;
+		BufferedImage segment = null;
 		try {
-			img1 = ImageIO.read(new File(fileLocationFirstPic));
-			img2 = ImageIO.read(new File(fileLocationSecondPic));
+			basis = ImageIO.read(new File(fileLocationBasicPicture));
+			rotated = ImageIO.read(new File(fileLocationRotatedPicture));
+			segment = ImageIO.read(new File(fileLocationSegmentPicture));
 		} catch (IOException e) {
 		System.out.println(e.getMessage());
 			throw new RuntimeException("Konnte Bild nicht einlesen");
 		}
 		
-		List<Mittelpunkt> list1= Aufgabe3Fingerpint.aufgabe3Fingerprint(img1);
-		List<Mittelpunkt> list2= Aufgabe3Fingerpint.aufgabe3Fingerprint(img2);
-		if(Aufgabe3FingerPrintTest.vergleiche(list1, list2)) {
-			System.out.println("Die beiden Bilder sind identisch");
+		List<Mittelpunkt> listBasis= Aufgabe3Fingerpint.aufgabe3Fingerprint(basis);
+		List<Mittelpunkt> listRotated= Aufgabe3Fingerpint.aufgabe3Fingerprint(rotated);
+		if(Aufgabe3FingerPrintTest.vergleiche(listBasis, listRotated)) {
+			System.out.println("Die beiden Bilder sind identisch oder ein Ausschnitt des anderen");
+		}else {
+			System.out.println("Es konnte keine Übereinstimmung gefunden werden");
 		}
 		
-		//vergleiche Mittelpunkte
-
+		
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		System.out.print("\n\n\n\n\n");
+		List<Mittelpunkt> listSegment = Aufgabe3Fingerpint.aufgabe3Fingerprint(segment);
+		if(Aufgabe3FingerPrintTest.vergleiche(listSegment, listBasis)) {
+			System.out.println("Die beiden Bilder sind identisch oder ein Ausschnitt des anderen");
+		}else {
+			System.out.println("Es konnte keine Übereinstimmung gefunden werden");
+		}
 	}
 	
-	public static boolean vergleiche(List<Mittelpunkt> list1, List<Mittelpunkt> list2) {
+	/**
+	 * 
+	 * @param listImage Liste der Mittelpunkte, die mit dem Basis Bild verglichen werden sollen
+	 * @param listBasis Liste der Mittelpunkte des Basisimage
+	 * @return true, wenn identisch/enthalten
+	 */
+	public static boolean vergleiche(List<Mittelpunkt> listImage, List<Mittelpunkt> listBasis) {
 		int count = 0;
 		//Mittelpunkt
-		for(int i = 0; i< list1.size(); i++) {
-			Mittelpunkt m = list1.get(i);
+		for(int i = 0; i< listImage.size(); i++) {
+			Mittelpunkt m = listImage.get(i);
 			//alle Entfernungen zu anderen Mittelpunkten
-			for(int k = 0; k< list2.size(); k++) {
-				if(vergleicheMittelpunkte(m, list2.get(k))) {
+			for(int k = 0; k< listBasis.size(); k++) {
+				if(vergleicheMittelpunkte(m, listBasis.get(k))) {
 					//wenn identischen Mittelpunkt gefunden, dann zähle mit
 					count++;
+					System.out.println("Übereinstimmenden Mittelpunkt für Mittelpunkt " + i + " gefunden");
 					break;
 				}
 			}
 			
 		}
 		//wenn genauso oft Mittelpunkt gefunden, wie Mittelpunkte in liste1, dann zu jedem mittelpunkt den passenden gefunden
-		if(count == list1.size()) {
+		if(count == listImage.size()) {
 			return true;
 		}
 			
@@ -67,14 +95,14 @@ public class Aufgabe3FingerPrintTest {
 	 * @param m2
 	 * @return
 	 */
-	private static boolean vergleicheMittelpunkte(Mittelpunkt m1, Mittelpunkt m2) {
+	private static boolean vergleicheMittelpunkte(Mittelpunkt punktImage, Mittelpunkt punktBasis) {
 
 		//iteriere über alle Entfernungen von m1
-		for(int i = 0; i<m1.entfernungen.size(); i++) {
-			double m1wert = m1.entfernungen.get(i);
+		for(int i = 0; i<punktImage.entfernungen.size(); i++) {
+			double imageWert = punktImage.entfernungen.get(i);
 			boolean gefunden = false;
-			for(int k = 0; k < m2.entfernungen.size(); k++) {
-				if(m1wert == m2.entfernungen.get(k)) {
+			for(int k = 0; k < punktBasis.entfernungen.size(); k++) {
+				if(imageWert == punktBasis.entfernungen.get(k)) {
 					gefunden = true;
 					break;
 				}
@@ -83,9 +111,6 @@ public class Aufgabe3FingerPrintTest {
 				return false;
 			}
 		}
-		
-		
-		
 		return true;
 	}
 }
